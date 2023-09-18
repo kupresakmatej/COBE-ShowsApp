9d45278816e24e03cae540f2ce474bf92401776d
Matej Kuprešak <matejkupresak@Matejs-MacBook-Pro.local>
2023-09-18 10:37:01 +0200

    Initial project commit

---
+++ ShowsApp/ShowsApp/ShowsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowsAppApp.swift
+//  ShowsApp
+//
+//  Created by Matej Kuprešak on 18.09.2023..
+//
+
+import SwiftUI
+
+@main
+struct ShowsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
