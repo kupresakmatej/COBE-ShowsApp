1f4fae7610dfefef07918d321c1e5a115d25c37a
kupresakmatej <matej.kupresak@gmail.com>
2023-09-22 11:31:12 +0200

    Migrate to uikit lifecycle

---
+++ Shows/Shows/RootView.swift
@@ -0,0 +1,22 @@
+//
+//  RootView.swift
+//  Shows
+//
+//  Created by Matej Kuprešak on 22.09.2023..
+//
+
+import SwiftUI
+
+
+struct RootView: View {
+    var body: some View {
+        SearchView()
+    }
+}
+
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
--- Shows/Shows/ShowsApp.swift
+++ Shows/Shows/ShowsApp.swift
@@ -7,11 +7,10 @@
 
 import SwiftUI
 
-@main
 struct ShowsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
