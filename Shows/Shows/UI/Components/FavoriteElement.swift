cd2781b2d3d730888b70aed46abe3a341e59f835
kupresakmatej <matej.kupresak@gmail.com>
2023-10-03 12:39:17 +0200

    Started the design of details view

---
+++ Shows/Shows/UI/Components/FavoriteElement.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteElement.swift
+//  Shows
+//
+//  Created by Matej Kuprešak on 03.10.2023..
+//
+
+import SwiftUI
+
+struct FavoriteElement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteElement_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteElement()
+    }
+}
